437f79f5e79303815292a99ed698b3c071fd99cd
Sidney Jeong <65862024+sid0831@users.noreply.github.com>
2020-11-05 09:13:00 +0900

    Version marking change.

--- commit.sh
+++ commit.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 VERSINFO=$(cat $PWD/pivotmonitor.sh | grep -iE 'Monitor pivot script v' | sed -E 's/([ \t].*)(echo -e "Monitor pivot script v)(.*)(\\nW.*)/\3/g')
-VERSARRAY=( $(echo "$VERSINFO" | sed -E 's/([0-9]*)(\.)([0-9]*)(\.)([0-9]*)(\-)([0-9]*)(\.)([0-9]*)/\1/') $(echo "$VERSINFO" | sed -E 's/([0-9]*)(\.)([0-9]*)(\.)([0-9]*)(\-)([0-9]*)(\.)([0-9]*)/\3/') $(echo "$VERSINFO" | sed -E 's/([0-9]*)(\.)([0-9]*)(\.)([0-9]*)(\-)([0-9]*)(\.)([0-9]*)/\5/') $(echo "$VERSINFO" | sed -E 's/([0-9]*)(\.)([0-9]*)(\.)([0-9]*)(\-)([0-9]*)(\.)([0-9]*)/\7/') $(echo "$VERSINFO" | sed -E 's/([0-9]*)(\.)([0-9]*)(\.)([0-9]*)(\-)([0-9]*)(\.)([0-9]*)/\9/') )
-
+VERSARRAY=( $(echo $VERSINFO | cut -d "-" -f 1 | cut -d "." -f 1) $(echo $VERSINFO | cut -d "-" -f 1 | cut -d "." -f 2) $(echo $VERSINFO | cut -d "-" -f 1 | cut -d "." -f 3) $(echo $VERSINFO | cut -d "-" -f 2 | cut -d "." -f 1) $(echo $VERSINFO | cut -d "-" -f 2 | cut -d "." f 2)  )
 commitcode () {
   local QMARK=1
   git commit -m "$2" $1; QMARK=$?
@@ -14,7 +13,7 @@ verschange () {
   local COMMITCOUNT=$(git rev-list --count master)
   VERSARRAY[4]=$(( $COMMITCOUNT + 1 ))
   echo -e "Marking new version...\nOLD: v$VERSINFO\nNEW: v${VERSARRAY[0]}.${VERSARRAY[1]}.${VERSARRAY[2]}-${VERSARRAY[3]}.${VERSARRAY[4]}"
-  sed -E -i "s/([0-9]*)(\.)([0-9]*)(\.)([0-9]*)(\-)([0-9]*)(\.)([0-9]*)/${VERSARRAY[0]}.${VERSARRAY[1]}.${VERSARRAY[2]}\-${VERSARRAY[3]}.${VERSARRAY[4]}/g" $PWD/pivotmonitor.sh; QMARK=$?
+  sed -E -i "s/(Monitor pivot script v)(.*)(\\nW.*)/Monitor pivot script v${VERSARRAY[0]}.${VERSARRAY[1]}.${VERSARRAY[2]}\-${VERSARRAY[3]}.${VERSARRAY[4]}\\n Written by Sidney Jeong, GNU GPL 3.0/g" $PWD/pivotmonitor.sh; QMARK=$?
   return $QMARK
 }
 
